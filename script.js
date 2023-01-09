fe9929345b6683098a7e65ce10cab228cd3d1129
GuglielmoBini <guglielmobini@gmail.com>
2023-01-09 16:42:38 +0100

    printed text and img to html

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Vue ok", Vue);
+const app = Vue.createApp({
+  data() {
+    return {
+      message:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae expedita labore odit fuga enim accusantium dicta quia. Cupiditate dolores sequi aperiam mollitia delectus deserunt ipsum tenetur eligendi cumque, ducimus corrupti?",
+      pic: {
+        img: "https://vuejs.org/images/logo.png",
+        alt: "logo",
+      },
+    };
+  },
+});
+
+app.mount("#root");
